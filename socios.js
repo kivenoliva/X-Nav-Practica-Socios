b99083f48245c4583ef0d88dedf93ab8e5019fe7
Kevin Oliva Muñoz <kivenoliva@gmail.com>
2015-03-16 18:18:06 +0100

    cambios para jquery

--- socios.js
+++ socios.js
@@ -1,1 +1,12 @@
-console.log("hola");
+var mensajesPendientes = 0;
+
+jQuery(document).ready(function() {
+    $(function() {
+        $( "#tabs" ).tabs();
+    });
+    
+    $.getJSON("json-update.json", function(data) {
+        pendientes = data.update.length;
+        $( ".jumbotron" ).prepend("<p>" + "Mensajes pendientes:  " + pendientes + "</p>");
+  });
+});
